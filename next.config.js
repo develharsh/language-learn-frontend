766e7cee663e95b33c0601e103e3dd29b0475c98
unknown <hvsreal.223@gmail.com>
2022-09-24 01:19:55 +0530

    added origin

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = {
   env: {
     // MONGODB_URL:
       // "mongodb+srv://hvs:thakur@cluster0.dqdk6.mongodb.net/production",
-    baseUrl: "http://localhost:3001",
-    // baseUrl: "https://theharshvs.herokuapp.com",
+    // baseUrl: "http://localhost:3001",
+    baseUrl: "https://languate.herokuapp.com",
   },
 };
